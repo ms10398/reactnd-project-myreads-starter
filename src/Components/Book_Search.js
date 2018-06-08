2e219b2a0796cc90cfa646abf950f8b81ae5699a
Mohit Sharma <msvbs98@gmail.com>
2018-06-09 00:39:42 +0530

    Project Completed

---
+++ src/Components/Book_Search.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import Book from './Book'
+import {PropTypes} from 'prop-types'
+import * as BooksAPI from '../utils/BooksAPI'
+
+class BookSearch extends Component {
+  state = {
+    Books: [],
+    query: ''
+  }
+
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    myBooks: PropTypes.array.isRequired
+  }
+
+  handleChange = (event) => {
+    let val = event.target.value
+    this.setState({ query: val })
+    this.searchBooks(val)
+  }
+
+  changeBookShelf = (books) => {
+    let allBooks = this.props.myBooks
+
+    for (let book of books) {
+      for (let b of allBooks) {
+        if (b.id === book.id) {
+          book.shelf = b.shelf
+        }
+      }
+    }
+    return books
+  }
+
+  searchBooks = (val) => {
+    if (val.length !== 0) {
+      BooksAPI.search(val, 10).then((books) => {
+        if (books.length > 0) {
+          books = this.changeBookShelf(books)
+          this.setState({Books: books})
+        }
+      })
+    } else {
+      this.setState({Books: [], query: ''})
+    }
+  }
+
+  addBook = (book, shelf) => {
+    this.props.onChange(book, shelf)
+  }
+
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to='/' className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleChange}/>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.query.length > 0 && this.state.Books.map((book, index) => (
+              <Book
+                book={book}
+                key={index}
+                onUpdate={(shelf) => {
+                  this.addBook(book, shelf)
+                }}
+              />
+              ))
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookSearch;
